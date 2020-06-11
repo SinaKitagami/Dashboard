ebe73136794b6fd324fda378f82e378f034dbef1
peachparfait <peachparfait10@gmail.com>
2020-06-11 20:49:15 +0900

    settings.pyなどをignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Mock データ
+const list = [
+    {
+        id: 1,
+        title: "1st todo",
+        description: "Learn Django properly."
+    },
+    {
+        id: 2,
+        title: "Second item",
+        description: "Learn Python."
+    },
+    {
+        id: 3,
+        title: "Learn HTTP",
+        description: "It's important."
+    }
+]
+
+class App extends Component {
+    constructor (props) {
+        super(props);
+        this.state = { list };
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.list.map(item => (
+                    <div key={item.id}>
+                        <h1>{item.title}</h1>
+                        <p>{item.description}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
